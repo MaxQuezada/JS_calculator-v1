cf4c4736caadc32b1d33865932c244f26588a941
Max Quezada <maxq2882@gmail.com>
2016-02-04 20:29:51 -0500

    *Update* finished the style sheet, had to chase a bug in the script.

--- Key Functions.js
+++ Key Functions.js
@@ -1,3 +1,79 @@
 /**
  * Created by The-Rig on 2/2/2016.
  */
+
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '÷', '-', 'x'];
+var decimalAdded = false;
+
+
+for(var i = 0; i < keys.length; i++) {
+    keys[i].onclick = function(e) {
+
+        var input = document.querySelector('.screen');
+        var inputVal = input.innerHTML;
+        var btnVal = this.innerHTML;
+
+
+        if (btnVal == 'C') {
+            input.innerHTML = '';
+            decimalAdded = false;
+        }
+
+
+        else if (btnVal == '=') {
+            var equation = inputVal;
+            var lastChar = equation[equation.length - 1];
+
+
+            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+
+
+            if (operators.indexOf(lastChar) > -1 || lastChar == '.')
+                equation = equation.replace(/.$/, '');
+
+            if (equation)
+                input.innerHTML = equals(equation);
+
+            decimalAdded = false;
+        }
+
+
+        else if (operators.indexOf(btnVal) > -1) {
+
+            var lastChar = inputVal[inputVal.length - 1];
+
+
+            if (inputVal != '' && operators.indexOf(lastChar) == -1)
+                input.innerHTML += btnVal;
+
+
+            else if (inputVal == ''&& btnVal == '-')
+                input.innerHTML += btnVal;
+
+
+            if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+
+                input.innerHTML = inputVal.replace(/.$/, btnVal);
+            }
+
+            decimalAdded = false;
+        }
+
+
+        else if(btnVal == '.') {
+            if(!decimalAdded) {
+                input.innerHTML += btnVal;
+                decimalAdded = true;
+            }
+        }
+
+
+        else {
+            input.innerHTML += btnVal;
+        }
+
+
+        e.preventDefault();
+    }
+}
